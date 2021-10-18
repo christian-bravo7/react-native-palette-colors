562f26afe120dba73c83c8970f4b9c2bb620b685
Christian Bravo <christian151.bravo@gmail.com>
2021-10-17 19:04:34 -0600

    boxes of different colors excercise

--- App.js
+++ App.js
@@ -11,29 +11,62 @@ import {
 const App = () => {
   return (
     <SafeAreaView style={[styles.safeArea, styles.androidSafeArea]}>
-      <View style={[styles.container, styles.yellow]}>
-        <Text>Hello world!</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Here are some boxes of different colors
+        </Text>
+        <View style={[styles.box, styles.cyan]}>
+          <Text style={styles.boxText}>Cyan #2aa198</Text>
+        </View>
+        <View style={[styles.box, styles.blue]}>
+          <Text style={styles.boxText}>Blue #268bd2</Text>
+        </View>
+        <View style={[styles.box, styles.magenta]}>
+          <Text style={styles.boxText}>Magenta #d33682</Text>
+        </View>
+        <View style={[styles.box, styles.orange]}>
+          <Text style={styles.boxText}>Orange #cb4b16</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  yellow: {
-    backgroundColor: 'yellow',
-  },
-  container: {
-    margin: 10,
-    flex: 1,
-    paddingHorizontal: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   safeArea: {
     flex: 1,
   },
   androidSafeArea: {
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  container: {
+    padding: 12,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginBottom: 12,
+  },
+  box: {
+    padding: 20,
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  boxText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
   },
 });
 
