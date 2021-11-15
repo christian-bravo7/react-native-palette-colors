c0e561063d4cd02a9824f2a08975a8563435c62d
Christian Bravo <christian151.bravo@gmail.com>
2021-11-14 18:52:13 -0600

    fetching palettes from api

--- screens/Home.js
+++ screens/Home.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
-import { COLORS, FRONTEND_MASTERS, RAINBOW } from '../api/colors';
+import React, { useCallback, useEffect, useState } from 'react';
+import { FlatList, View } from 'react-native';
 import PalettePreviewNavigation from '../components/PalettePreviewNavigation';
 
+const endpoint = 'https://color-palette-api.kadikraman.now.sh/palettes';
+
 const Home = ({ navigation }) => {
+  const [palettes, setPalettes] = useState([]);
+
+  const fetchColorPalettes = useCallback(async () => {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    setPalettes(data);
+  }, []);
+
+  useEffect(() => {
+    fetchColorPalettes();
+  }, [fetchColorPalettes]);
+
   return (
-    <View>
-      <PalettePreviewNavigation
-        navigation={navigation}
-        colors={COLORS}
-        title="Solarized"
-      />
-      <PalettePreviewNavigation
-        navigation={navigation}
-        colors={RAINBOW}
-        title="Rainbow"
-      />
-      <PalettePreviewNavigation
-        navigation={navigation}
-        colors={FRONTEND_MASTERS}
-        title="Frontend masters"
-      />
-    </View>
+    palettes.length > 0 && (
+      <View>
+        <FlatList
+          data={palettes}
+          keyExtractor={({ paletteName }) => paletteName}
+          renderItem={({ item }) => (
+            <PalettePreviewNavigation
+              key={item.id}
+              navigation={navigation}
+              colors={item.colors}
+              title={item.paletteName}
+            />
+          )}
+        />
+      </View>
+    )
   );
 };
 
